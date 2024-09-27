80faa8fc9554e22389092ab366a8ac545dc4824c
Mosi Zamani <eng.mzamanit@gmail.com>
2024-09-27 09:55:13 -0700

    working on a task app

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -3,14 +3,19 @@ import { useState } from 'react'
 export default function AddTask({ onAddTask}) {
     const [text, setText] = useState('')
 
+    return (
+        <>
+            <input 
+            placeholder="Add task..."
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            />
+            <button onClick={() => {
+                setText('')
+                onAddTask(text)
+                }}>
+                Add
+            </button>
+        </>
+    )
 }
-
-return (
-    <>
-        <input 
-        placeholder="Add task..."
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-        />
-    </>
-)
